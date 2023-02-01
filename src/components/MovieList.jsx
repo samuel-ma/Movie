f898c5ee6f9c702e7f3cda85e7be355d9dca7910
samuel-ma <mabilmakur@gmail.com>
2023-02-01 15:18:16 +0200

    Testing API Protocols

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,24 +1,22 @@
+import axios from 'axios';
 import {useState, useEffect} from 'react'
-import MovieCard from './MovieCard';
 import tmdb from '../api/tmdb';
+import MovieCard from './MovieCard';
 
 const MovieList = () => {
 
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const fetchMovies = async() => {
-      const {data} = await tmdb.get("tv/popular")
-      setMovies(data.results)
-    }
-
-    fetchMovies();
+    const fetchMovies = axios.get("https://api.themoviedb.org/3/movie/550?api_key=a3985284ae4c1570e3b3123fced85f63").then((res) => console.log(res)).catch((err) => console.log(err));
+    console.log(fetchMovies);
   }, [])
-  
+
+
   return (
     <div>
-      {movies.map((movie,index) => {
-        return <MovieCard key={index} {...movie} />
+      {movies.map((items) => {
+        <MovieCard/>
       })}
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,47 +2,39 @@ import React from 'react'
 import "../styles/Navbar.css"
 import { AiFillClockCircle } from "react-icons/ai";
 import { RiMacFill, RiHome6Fill, RiLogoutCircleRFill, RiMovie2Fill } from "react-icons/ri";
-import { FaCalendarAlt } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div className='navbar'>
 
-      <div className='topnav'>
-        EyeFlix
+      <div className='nav'>
+        <div className='topnav'>
+          EyeFlix
+        </div>
+        
+        <div className='midnav'>
+          <div className='navicons'>
+            <RiHome6Fill/>
+          </div>
+
+          <div className='navicons'>
+            <RiMovie2Fill/>
+          </div>
+
+          <div className='navicons'>
+            <RiMacFill/>
+          </div>
+
+          <div className='navicons'>
+            <AiFillClockCircle/>
+          </div>
+
+        </div>
+
+        <div className='navicons'>
+            <RiLogoutCircleRFill/>
+          </div>
       </div>
-      
-      <div className='midnav'>
-        <div className='navicons'>
-          <RiHome6Fill/>
-          <p className='navt'>Home</p>
-        </div>
-
-        <div className='navicons'>
-          <RiMovie2Fill/>
-          <p className='navt'>Movie</p>
-        </div>
-
-        <div className='navicons'>
-          <RiMacFill/>
-          <p className='navt'>TV</p>
-        </div>
-
-        <div className='navicons'>
-          <AiFillClockCircle/>
-          <p className='navt'>Watch</p>
-        </div>
-
-        <div className='navicons'>
-          <FaCalendarAlt/>
-          <p className='navt'>History</p>
-        </div>
-      </div>
-
-      <div className='navicons'>
-          <RiLogoutCircleRFill/>
-          <p className='navt'>Logout</p>
-        </div>
 
     </div>
   )
