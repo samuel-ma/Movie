63a8bcced01479731d5cd06f1b0f9dd90ed2b64b
samuel-ma <mabilmakur@gmail.com>
2023-02-07 12:17:04 +0200

    Connected the API to component successfully

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 import "../styles/MovieCard.css"
 import { BsStarFill } from "react-icons/bs";
 
-const MovieCard = ({title, description, posterURL, rating}) => {
+const MovieCard = ({movie}) => {
   return (
     <div className="card">
-        <img className="poster" src={posterURL} alt="image" />
-        <h1 className="title">{title}</h1>
-        <p className="descr">{description}</p>
+        <img className="poster" src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt="image" />
+        <h1 className="title">{movie.title}</h1>
+        <p className="descr">{movie.overview.length < 100 ? movie.overview : `${movie.overview.slice(0, 100)}...`}</p>
         <div className="rate">
-          <p className="rating">{rating}</p>
+          <p className="rating">{movie.vote_average}</p>
           <BsStarFill/>
         </div>
     </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,60 +1,27 @@
-// import axios from 'axios';
-// import {useState, useEffect} from 'react'
-// import tmdb from '../api/tmdb';
+import {useState, useEffect} from 'react'
+import tmdb from '../api/tmdb';
 import "../styles/MovieList.css"
 import MovieCard from './MovieCard';
-import p1 from "../assets/p (1).jpg";
-import p2 from "../assets/p (2).jpg";
-import p3 from "../assets/p (3).jpg";
-import p4 from "../assets/p (4).jpg";
-import p5 from "../assets/p (5).jpg";
-import p6 from "../assets/p (6).jpg";
-import p7 from "../assets/p (7).jpg";
-import p8 from "../assets/p (8).jpg";
 
 const MovieList = () => {
 
-  // const [movies, setMovies] = useState([]);
+  const [popular, setPopular] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchMovies = axios.get("https://api.themoviedb.org/3/movie/550?api_key=a3985284ae4c1570e3b3123fced85f63").then((res) => console.log(res)).catch((err) => console.log(err));
-  //   console.log(fetchMovies);
-  // }, [])
+  useEffect(() => {
+    fetchPopular();
+}, [])
 
-  const movies = {
-      title: "Narnia",
-      rating: 4.5,
-      description: "This is the desscription of the above movie whatever it might be"
-  }
-
-  const movies2 = {
-      title: "Black Adam",
-      rating: 4.2,
-      description: "This is the desscription of the above movie whatever it might be"
-  }
-
-  const movies3 = {
-      title: "Glass Onion",
-      rating: 4.8,
-      description: "This is the desscription of the above movie whatever it might be"
-  }
-
-  const movies4 = {
-      title: "Devotion",
-      rating: 3.5,
-      description: "This is the desscription of the above movie whatever it might be"
+const fetchPopular = async() => {
+    const data = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=a3985284ae4c1570e3b3123fced85f63&language=en-US&page=1")
+    const movies = await data.json()
+    setPopular(movies.results)
   }
 
   return (
     <div className="list">
-        <MovieCard title={movies.title} posterURL={p1} rating={movies.rating} description={movies.description} />
-        <MovieCard title={movies2.title} posterURL={p2} rating={movies2.rating} description={movies2.description} />
-        <MovieCard title={movies3.title} posterURL={p3} rating={movies3.rating} description={movies3.description} />
-        <MovieCard title={movies4.title} posterURL={p4} rating={movies4.rating} description={movies4.description} />
-        <MovieCard title={movies.title} posterURL={p5} rating={movies.rating} description={movies.description} />
-        <MovieCard title={movies2.title} posterURL={p6} rating={movies2.rating} description={movies2.description} />
-        <MovieCard title={movies.title} posterURL={p7} rating={movies.rating} description={movies.description} />
-        <MovieCard title={movies2.title} posterURL={p8} rating={movies2.rating} description={movies2.description} />
+        {popular.map(movie => {
+            return <MovieCard key={movie.id} movie={movie}/>
+        })}
     </div>
   )
 }
