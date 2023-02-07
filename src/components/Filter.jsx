aa92078ce96d2fc6fca46409f1ff62e1208e1b3b
samuel-ma <mabilmakur@gmail.com>
2023-02-07 14:45:36 +0200

    More commits

---
+++ src/components/Filter.jsx
@@ -0,0 +1,14 @@
+
+import "../styles/Filter.css"
+import { useEffect } from "react"
+import { FaTags } from "react-icons/fa";
+
+function Filter({setActive}) {
+  return (
+    <button className="filt" onClick={() => setActive(35)}>
+              <FaTags />
+    </button>
+  )
+}
+
+export default Filter
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,6 +6,8 @@ import MovieCard from './MovieCard';
 const MovieList = () => {
 
   const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [active, setActive] = useState(0);
 
   useEffect(() => {
     fetchPopular();
@@ -15,6 +17,7 @@ const fetchPopular = async() => {
     const data = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=a3985284ae4c1570e3b3123fced85f63&language=en-US&page=1")
     const movies = await data.json()
     setPopular(movies.results)
+    setFiltered(movies.results)
   }
 
   return (
