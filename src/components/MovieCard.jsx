08081b1a5308dc95dee5eaae2bb4638b7f09c2f2
samuel-ma <mabilmakur@gmail.com>
2023-02-08 05:53:46 +0200

    more commits

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const MovieCard = ({movie}) => {
     <div className="card">
         <img className="poster" src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt="image" />
         <h1 className="title">{movie.title}</h1>
-        <p className="descr">{movie.overview.length < 100 ? movie.overview : `${movie.overview.slice(0, 100)}...`}</p>
+        <p className="descr">{movie.overview.length < 130 ? movie.overview : `${movie.overview.slice(0, 130)}...`}</p>
         <div className="rate">
           <p className="rating">{movie.vote_average}</p>
           <BsStarFill/>
