28701a8fd6484aba517d74e4f67ac5006da71e65
samuel-ma <mabilmakur@gmail.com>
2023-02-10 12:58:04 +0200

    More commits

--- src/Pages/Television.jsx
+++ src/Pages/Television.jsx
@@ -1,9 +1,15 @@
 
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Television() {
   return (
-    <div>Television</div>
+    <div>
+      <Link to="home">Home page</Link>
+      <Link to="saved">Saved page</Link>
+      <Link to="television">Television page</Link>
+      <Link to="movies">Movies page</Link>
+      <h1>Saved</h1>
+    </div>
   )
 }
 
